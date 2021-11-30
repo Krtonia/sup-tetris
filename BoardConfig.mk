94484af2af61706bc0def9128f7be2e6cd4294c0
TheMalachite <eliasgheeraert@gmail.com>
2021-11-30 17:10:35 +0100

    tetris: sepolicy: Address AOSP audio hal denials
    Change-Id: Ide07c0baebd702766ec2d061c7d3adc2f8383668

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -131,6 +131,7 @@ VENDOR_SECURITY_PATCH := 2024-05-05
 
 # Sepolicy
 -include device/mediatek/sepolicy_vndr/SEPolicy.mk
+BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 
 # Verified Boot
 BOARD_AVB_ENABLE := true
