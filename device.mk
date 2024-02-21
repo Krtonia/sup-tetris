88c234ecffba13346cd9d8afd639d5d532c11b4d
Kshitij Gupta <kshitijgm@gmail.com>
2024-02-21 18:49:09 +0530

    tetris: props: Kill vendor logspam
    * Vendor services spam logcat more than one would like.
      On user builds, most logging is automatically disabled.
    
    * Given almost everything works now, suppress these logs and
      enable them only on eng builds.
    
    Change-Id: I8b0c7d70f19ac2d2085973dae8e17642207e345b
    Co-Authored-By: Woomymy <woomy@woomy.be>
    Signed-off-by: Sarthak Roy <sarthakroy2002@gmail.com>
    Signed-off-by: Sarthak Roy <krtonia@gmail.com>

--- device.mk
+++ device.mk
@@ -23,6 +23,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/generic_ramdisk.mk)
 # Setup dalvik vm configs
 $(call inherit-product,frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 
+# Add vendor log tags
+include $(LOCAL_PATH)/configs/properties/vendor_log_tags.mk
+
 # A/B
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
