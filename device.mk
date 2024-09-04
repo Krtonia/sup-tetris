634c46832b19156641abee2e9d94744f48dcbd33
SahilSonar <sss.sonar2003@gmail.com>
2024-09-04 18:50:24 +0530

    tetris: Build libsuspend for KPOC

--- device.mk
+++ device.mk
@@ -57,6 +57,10 @@ BOARD_SHIPPING_API_LEVEL := 34
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 
+# Charger
+PRODUCT_PACKAGES += \
+    libsuspend
+
 # Radio
 SKIP_BOOT_JARS_CHECK := true
 
