e0d3ce4a4b914ed7a4d748bc24d285d4a0c35174
SahilSonar <sss.sonar2003@gmail.com>
2024-09-04 18:29:08 +0530

    tetris: Initial skeleton tree

---
+++ lineage_tetris.mk
@@ -0,0 +1,29 @@
+
+#
+# Copyright (C) 2022 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit common lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Inherit from tetris device
+$(call inherit-product, device/nothing/tetris/device.mk)
+
+PRODUCT_NAME := lineage_tetris
+PRODUCT_DEVICE := tetris
+PRODUCT_BRAND := Nothing
+PRODUCT_MANUFACTURER := Nothing
+PRODUCT_MODEL := A015
+
+PRODUCT_GMS_CLIENTID_BASE := android-nothing
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="Tetris 14 UP1A.231005.007 2407291047 release-keys"
+
+BUILD_FINGERPRINT := Nothing/Tetris/Tetris:14/UP1A.231005.007/2407291047:user/release-keys
