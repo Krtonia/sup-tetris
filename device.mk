b8e7bbf551cb665bd34725ddcab43eeb0a2b6c2c
SahilSonar <sss.sonar2003@gmail.com>
2024-09-04 18:41:52 +0530

    tetris: Include 6GB dalvik heap configuration

--- device.mk
+++ device.mk
@@ -8,6 +8,9 @@ BOARD_BUILD_SUPER_IMAGE_BY_DEFAULT := true
 PRODUCT_BUILD_SUPER_PARTITION := true
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
+# Setup dalvik vm configs
+$(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
+
 # Shipping API
 PRODUCT_SHIPPING_API_LEVEL := 34
 BOARD_SHIPPING_API_LEVEL := 34
