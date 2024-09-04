7c67be866daec2fd9972a089ed425d93c22f2a83
SahilSonar <sss.sonar2003@gmail.com>
2024-09-04 18:33:14 +0530

    tetris: Add kernel flags

--- device.mk
+++ device.mk
@@ -12,6 +12,9 @@ BOARD_SHIPPING_API_LEVEL := 34
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 
+# Kernel
+$(call inherit-product, device/nothing/tetris-kernel/kernel.mk)
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
