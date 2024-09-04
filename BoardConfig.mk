e6f7af5448961197ad02e1b698fb62ba233e7b66
SahilSonar <sss.sonar2003@gmail.com>
2024-09-04 18:46:31 +0530

    tetris: Include mtk sepolicy

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -107,6 +107,9 @@ TARGET_RECOVERY_PIXEL_FORMAT := RGBX_8888
 TARGET_RECOVERY_UI_MARGIN_HEIGHT := 100
 TARGET_USERIMAGES_USE_F2FS := true
 
+# Sepolicy
+-include device/mediatek/sepolicy_vndr/SEPolicy.mk
+
 # Verified Boot
 BOARD_AVB_ENABLE := true
 BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 3
