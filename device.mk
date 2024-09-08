7b6272cf7f2845657dfe4fc006d3d8f5687cfb87
SahilSonar <sss.sonar2003@gmail.com>
2024-09-08 14:28:28 +0530

    tetris: Import public libraries file

--- device.mk
+++ device.mk
@@ -238,6 +238,10 @@ PRODUCT_COPY_FILES += \
 include $(LOCAL_PATH)/vendor_props.mk
 PRODUCT_COMPATIBLE_PROPERTY_OVERRIDE := true
 
+# Public libraries
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
+
 # Radio
 PRODUCT_PACKAGES += \
     android.hardware.radio-V2-ndk.vendor \
