6804012ad2bb3bbd239c76e1bc0ba641cb7006d5
SuperAviation001 <superaviation001@gmail.com>
2025-04-25 12:05:46 +1000

    Revert "tetris: Build required libraries for fingerprint service"
    This reverts commit 0680bd70ef156e402cdae229a8271f6edf1df217.

--- device.mk
+++ device.mk
@@ -11,6 +11,9 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 # Project ID Quota
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 
+# Fingerprint
+TARGET_HAS_UDFPS := true
+
 # Inherit virtual_ab_ota product
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch_with_vendor_ramdisk.mk)
 
@@ -186,13 +189,6 @@ PRODUCT_PACKAGES += \
     libhidltransport.vendor \
     libhwbinder \
     libhwbinder.vendor
-
-# Fingerprint
-TARGET_HAS_UDFPS := true
-
-PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint-V4-ndk.vendor \
-    android.hardware.biometrics.common.util.vendor \
 
 # Keymint
 PRODUCT_PACKAGES += \
