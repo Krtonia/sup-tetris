b9a64e06245b620b8972f3e9d3efa5add09c9eee
raghavt20 <raghavt20.github.io>
2025-04-25 12:07:40 +1000

    tetris: Build required libraries for fingerprint service
    android.hardware.biometrics.common.thread.vendor
    android.hardware.biometrics.common.util.vendor
    android.hardware.biometrics.fingerprint-V4-ndk.vendor
    
    Change-Id: Ifce36245686a9199da86500ca76edd65bb5de35a

--- device.mk
+++ device.mk
@@ -11,9 +11,6 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 # Project ID Quota
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 
-# Fingerprint
-TARGET_HAS_UDFPS := true
-
 # Inherit virtual_ab_ota product
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch_with_vendor_ramdisk.mk)
 
@@ -189,6 +186,13 @@ PRODUCT_PACKAGES += \
     libhidltransport.vendor \
     libhwbinder \
     libhwbinder.vendor
+# Fingerprint
+TARGET_HAS_UDFPS := true
+
+PRODUCT_PACKAGES += \
+    android.hardware.biometrics.fingerprint-V4-ndk.vendor \
+    android.hardware.biometrics.common.util.vendor \
+    android.hardware.biometrics.common.thread.vendor
 
 # Keymint
 PRODUCT_PACKAGES += \
