93d97b3eb0891f17032bd247f29eb88651242ebd
SuperAviation001 <superaviation001@gmail.com>
2025-06-02 21:51:01 +1000

    tetris: Automatically clone device/mediatek/sepolicy_vndr to its correct place
    Buildbot heaven

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -11,3 +11,6 @@ fi
 if [ ! -d "device/nothing/tetris-kernel" ]; then
   git clone https://github.com/SuperAviation001/android_device_nothing_tetris-kernel -b a15 device/nothing/tetris-kernel
 fi
+if [ ! -d "device/nothing/tetris-kernel" ]; then
+  https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr.git -b lineage-22.2 device/mediatek/sepolicy_vndr
+fi
