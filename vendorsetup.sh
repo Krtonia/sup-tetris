9e441e61c979e13c08d4c568ce3b9dae075ef106
Shikhar <Krtonia@gmail.com>
2025-06-15 00:12:32 +0530

    tetris: switch vendor remote

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -6,7 +6,7 @@ if [ ! -d "hardware/mediatek" ]; then
   git clone https://github.com/SuperAviation001/android_hardware_mediatek -b lineage-22.2 hardware/mediatek
 fi
 if [ ! -d "vendor/nothing/tetris" ]; then
-  git clone https://gitlab.com/krtonia/android_vendor_nothing_tetris.git -b a15 vendor/nothing/tetris
+  git clone https://gitlab.com/SuperAviation001/android_vendor_nothing_tetris -b a15 vendor/nothing/tetris
 fi
 if [ ! -d "kernel/nothing/tetris" ]; then
   git clone https://github.com/SuperAviation001/android_kernel_nothing_tetris -b lineage-22.2 kernel/nothing/tetris
